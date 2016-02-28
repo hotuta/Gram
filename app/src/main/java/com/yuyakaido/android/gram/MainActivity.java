22d1e120c5e3a8f4469e3fc3a30c93c64a5bae46
Yuya Kaido <yuyakaido@gmail.com>
2016-02-28 17:24:52 +0900

    Do authentication only once

--- app/src/main/java/com/yuyakaido/android/gram/MainActivity.java
+++ app/src/main/java/com/yuyakaido/android/gram/MainActivity.java
@@ -9,23 +9,39 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity
         implements InstagramAuthenticationDialog.InstagramAuthenticationCallback {
 
+    private static final String SP_KEY_INSTAGRAM_ACCESS_TOKEN = "SP_KEY_INSTAGRAM_ACCESS_TOKEN";
+
+    private View authenticateButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.activity_main_authentication_button)
-                .setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        showAuthenticationDialog();
-                    }
-                });
+
+        authenticateButton = findViewById(R.id.activity_main_authentication_button);
+
+        String accessToken = SharedPreferencesUtil.getString(this, SP_KEY_INSTAGRAM_ACCESS_TOKEN);
+        if (accessToken == null) {
+            authenticateButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    showAuthenticationDialog();
+                }
+            });
+        } else {
+            authenticateButton.setVisibility(View.GONE);
+            fetchInstagramMedias(accessToken);
+        }
     }
 
     @Override
     public void onInstagramAuthenticationCompleted(String accessToken) {
+        SharedPreferencesUtil.saveString(this, SP_KEY_INSTAGRAM_ACCESS_TOKEN, accessToken);
         Toast.makeText(MainActivity.this, accessToken, Toast.LENGTH_SHORT).show();
+        authenticateButton.setVisibility(View.GONE);
     }
+
+    private void fetchInstagramMedias(String accessToken) {}
 
     private void showAuthenticationDialog() {
         DialogFragment dialog = InstagramAuthenticationDialog.newInstance();
---
+++ app/src/main/java/com/yuyakaido/android/gram/SharedPreferencesUtil.java
@@ -0,0 +1,24 @@
+package com.yuyakaido.android.gram;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by yuyakaido on 2/28/16.
+ */
+public class SharedPreferencesUtil {
+
+    public static void saveString(Context context, String key, String value) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(key, value);
+        editor.apply();
+    }
+
+    public static String getString(Context context, String key) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return sharedPreferences.getString(key, null);
+    }
+
+}
